e5d7ddab1df5c3490d4daf720cfe2612021aa538
120591516 <120591516@qq.com>
2017-05-27 13:27:09 +0800

    a

--- src/main/java/cn/itcast/springboot/demo/HelloApplication.java
+++ src/main/java/cn/itcast/springboot/demo/HelloApplication.java
@@ -18,23 +18,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Configuration
 public class HelloApplication {
 
-    @RequestMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "hello world！ 传智播客！";
-    }
+	@RequestMapping("hello")
+	@ResponseBody
+	public String hello() {
+		return "hello world！ 传智播客！";
+	}
 
-    @Bean
-    public StringHttpMessageConverter stringHttpMessageConverter() {
-        StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("ISO-8859-1"));
-        return converter;
-    }
+	@Bean
+	public StringHttpMessageConverter stringHttpMessageConverter() {
+		StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UFT-8"));
+		return converter;
+	}
 
-    public static void main(String[] args) {
-        // SpringApplication.run(HelloApplication.class, args);
-        SpringApplication application = new SpringApplication(HelloApplication.class);
-        application.setBannerMode(Mode.OFF);
-        application.run(args);
-    }
+	public static void main(String[] args) {
+		// SpringApplication.run(HelloApplication.class, args);
+		SpringApplication application = new SpringApplication(HelloApplication.class);
+		application.setBannerMode(Mode.OFF);
+		application.run(args);
+	}
 
 }
